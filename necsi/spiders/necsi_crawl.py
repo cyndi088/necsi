01927e716d1196cc3fb33992c9b919c3c8b3f9f3
cyndi088 <cyndi088@163.com>
2018-06-13 22:29:00 +0800

    解决xpath返回空

--- necsi/spiders/necsi_crawl.py
+++ necsi/spiders/necsi_crawl.py
@@ -4,8 +4,12 @@ import scrapy
 
 class NecsiCrawlSpider(scrapy.Spider):
     name = 'necsi_crawl'
-    allowed_domains = ['http://www.necsi.edu/events/iccs2018/']
-    start_urls = ['http://http://www.necsi.edu/events/iccs2018//']
+    allowed_domains = ['www.necsi.edu/events/iccs2018']
+    start_urls = ['http://www.necsi.edu/events/iccs2018/']
 
     def parse(self, response):
-        pass
+        resp = response.xpath('//body/div[@class="row"][5]/div[@class="column"]/div[@class="row"]//p//b/text()').extract()
+        print('***************************')
+        print(resp)
+        print('***************************')
+
